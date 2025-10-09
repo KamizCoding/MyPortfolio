601fc80ec9d1d845cf9bb30735cad16902aa45f5
lukmaan kamiz <m.lukmaankamiz@gmail.com>
2025-10-09 20:15:33 +0530

    Finalized Home Structure ✅

--- src/app/sections/home.jsx
+++ src/app/sections/home.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaDownload } from "react-icons/fa";
 
 export default function HomePage() {
   return (
     <div className="flex items-center h-screen font-sans w-full bg-cover bg-center bg-no-repeat bg-[url('/images/homebg.png')]">
       <div className="flex flex-col pt-20 pl-40 space y-2">
-        <h1 className="flex text-6xl font-bold">Hellob,</h1>
+        <h1 className="flex text-6xl font-bold">Hello,</h1>
         <h1 className="text-4xl font-semibold">I'm <span className="text-5xl font-bold text-blue-300">Lukmaan Kamiss</span></h1>
         <p className="text-4xl font-bold">I'm a
           <span className="ml-3 slider-window" arisl-live="polite" role="status">
@@ -19,28 +19,36 @@ export default function HomePage() {
             </span>
           </span>
         </p>
-        <div className="flex">
-          <Link href="mycv.pdf" download={"Lukmaan_Kamiss_CV.pdf"}>
-            <button className="border-2 rounded-2xl w-40 h-9 mt-2 bg-blue-300 text-black hover:bg-blue-700 hover:text-gray-100 transition-colors duration-300 cursor-pointer">Download CV</button>
-          </Link>
-          <Link href="https://www.linkedin.com/in/lukmaan-kamiss?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BDD7CzKlAQ86m%2BdDZX5alhw%3D%3D">
-            <button className="mt-3 ml-3 border-2 p-1 rounded-full cokor"><FaLinkedin style={{ color: 'white' }} size={20} color="#0077b5" /> </button>
-          </Link>
-          <Link href="https://github.com/KamizCoding">
-            <button className="mt-3 ml-3 border-2 p-1 rounded-full cokor"><FaGithub style={{ color: 'white' }} size={20} color="#0077b5" /> </button>
-          </Link>
+        <div className="flex flex-col pt-5 pl-15">
+          <div>
+            <Link href="mycv.pdf" download={"Lukmaan_Kamiss_CV.pdf"}>
+              <button className="relative group overflow-hidden py-3 px-6 rounded text-white font-semibold transition-all duration-300 cursor-pointer">
+                <span className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-900 opacity-100 group-hover:opacity-0 transition-opacity duration-500"></span>
+                <span className="absolute inset-0 bg-gradient-to-r from-blue-900 to-blue-400 opacity-0 group-hover:opacity-100 transition-opacity duration-500"></span>
+                <span className="flex relative z-10 gap-2 pt-2 text-xl">Download CV <FaDownload/></span>
+              </button>
+            </Link>
+          </div>
+          <div className="pl-13">
+            <Link href="https://www.linkedin.com/in/lukmaan-kamiss?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BDD7CzKlAQ86m%2BdDZX5alhw%3D%3D">
+              <button className=" group mt-3 ml-3 border-2 p-1 rounded-full cursor-pointer hover:scale-110 hover:border-blue-400 hover:shadow-lg hover:shadow-blue-500/50 transition-all duration-300"><FaLinkedin className="text-white transition-colors duration-300 group-hover:text-blue-400" size={25}/> </button>
+            </Link>
+            <Link href="https://github.com/KamizCoding">
+              <button className="group mt-3 ml-3 border-2 p-1 rounded-full cursor-pointer hover:scale-110 hover:shadow-lg hover:shadow-blue-500/50 transiton-all duration-300"><FaGithub className="text-white transition-colors duration-300 group-hover:text-blue-400" size={25}/> </button>
+            </Link>
+          </div>
         </div>
       </div>
 
       <div className="relative flex items-center justify-center pl-20">
         <div className="absolute">
-          <div className="relative flex justify-center items-center w-[200px] h-[200px]">
-            <div className="shadow-lg shadow-blue-500 bg-transparent absolute rounded-full w-79 h-79 spin-right"></div>
-            <div className="shadow-lg shadow-violet-700 bg-transparent absolute rounded-full w-76 h-76 spin-left"></div>
-            <div className="shadow-lg shadow-cyan-500 bg-transparent absolute rounded-full w-75 h-75 spin-right-fast"></div>
+          <div className="relative flex justify-center items-center">
+            <div className="shadow-md shadow-blue-500 bg-transparent absolute rounded-full w-77 h-77 spin-right"></div>
+            <div className="shadow-md shadow-violet-700 bg-transparent absolute rounded-full w-76 h-76 spin-left"></div>
+            <div className="shadow-md shadow-cyan-500 bg-transparent absolute rounded-full w-75 h-75 spin-right-fast"></div>
           </div>
         </div>
-        <Image className="h-75 w-75 rounded-full border-2 border-gray-600"
+        <Image className="h-75 w-60 rounded-full"
           src="/images/mypic.png"
           height={100}
           width={300}
